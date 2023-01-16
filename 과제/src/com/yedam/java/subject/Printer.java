210dc046807b1a8dd96af84ee95bc83d156ff5c8
JjungEeunAae <wjddmsdo77@naver.com>
2023-01-16 23:17:09 +0900

    완료

--- 과제/src/com/yedam/java/subject/Printer.java
+++ 과제/src/com/yedam/java/subject/Printer.java
@@ -3,4 +3,16 @@ package com.yedam.java.subject;
 public class Printer {
 	// 책 269페이지 4번
 	// pintln()메소드 -> 매개값은 각 int, boolean, double, String을 줄 수 있습니다.
+	public void println(int value) {
+		System.out.println(vlaue);
+	}
+	public void println(boolean value) {
+		System.out.println(vlaue);
+	}
+	public void println(bouble value) {
+		System.out.println(vlaue);
+	}
+	public void println(String value) {
+		System.out.println(vlaue);
+	}
 }
