89e9c80d1d32e55458a07d7b4407bec4ecce48d7
Jung Eun Ae <wjddmsdo77@naver.com>
2023-01-18 17:05:46 +0900

    학원

---
+++ chap10/src/com/yedam/java/ch01/ExceptionExample.java
@@ -0,0 +1,59 @@
+package com.yedam.java.ch01;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) {
+		//NullPointerException
+		//오류가 났을 때 위치를 알려줌
+		//예외가 발생했을 때 메세지와 그게 실제로 발생한 코드를 매칭시켜보면 좀 더 찾아가기 쉽다
+		//String data = null;
+		//System.out.println(data.toCharArray());
+		
+		//ArrayIndexOutOfBoundsException
+		//실행할 때 값을 주느냐 안주느냐에 따라 실행 또는 에러가 발생함
+		
+		//if -> 예외처리
+//		if(args.length >2) {
+//		String data1 = args[0];
+//		String data2 = args[1];
+//		
+//		
+//		System.out.println("args[0] : " + data1);
+//		System.out.println("args[0] : " + data2);
+//		}
+		
+		
+		
+		//NumberFormatException
+//		String data3 = "100";
+//		String data4 = "a100";
+//		
+//		int value3 = Integer.parseInt(data3);
+//		int value4 = Integer.parseInt(data4);
+//		
+//		int result = value3 + value4;
+//		
+//		System.out.printf("%s + %s = %d\n",data3,data4,result);
+		
+		//ClassCastException
+		changeDog(new Dog());
+		changeDog(new Cat());
+		
+		
+	}
+	
+	//ExceptionExampl의 클래스
+	public static void changeDog(Animal animal) {
+		//상속관계가 성립되기 때문에 강제타입변환이 가능함
+		//에러는 밑에서부터 위로 올라간다.
+		if(animal instanceof Dog) {
+		Dog dog = (Dog)animal;
+		}
+	}
+	
+}
+
+////NumberFormatException
+class Animal{}
+class Dog extends Animal{}
+class Cat extends Animal{}
---
+++ chap10/src/com/yedam/java/ch02/TryCatchFinallyExample.java
@@ -0,0 +1,80 @@
+package com.yedam.java.ch02;
+
+import java.util.Scanner;
+
+public class TryCatchFinallyExample {
+
+	public static void main(String[] args) {
+		//일반 예외
+		try {
+			Class clazz = Class.forName("java.lang.String2");	//클래스라는 class가 있음(클래스 자체를 들고 있는 class)
+		} catch (ClassNotFoundException e) {
+			//catch는 모든 예외처리를 넣을 수 있다.
+			
+			//다른 프로그램과 통신할 때 통신이 불가한 이유가 무수하다.
+			//printStackTrace() -> 가장 정확한 예외의 원인을 알고 있는 메소드
+			//e.printStackTrace();
+			System.out.println("해당 클래스가 존재하지 않습니다");
+		}
+		
+		
+		String data1 = null;
+		String data2 = null;
+		//실행 예외
+		try {
+			data1 = args[0];
+			data2 = args[1];
+			
+			int value1 = Integer.parseInt(data1);
+			int value2 = Integer.parseInt(data2);
+			int result = value1 + value2;
+			System.out.printf("%s + %s = %d\n" , data1, data2, result);
+			
+			//강제로 해당 예외클래스를 생성하는 것
+			//=임의로 발생시킨다?
+			throw new ClassCastException();
+			
+		} catch(ArrayIndexOutOfBoundsException e){
+			//2개의 예외 발생
+			System.out.println("실행 매개값의 수가 부족합니다");
+		} catch(NumberFormatException e) {
+			System.out.println("숫자로 변환할 수 없습니다");
+		} catch(Exception e) {
+			System.out.println("정상적으로 실행되지 않습니다");
+		} finally {
+			//return을 하더라도 무조건 실행되는 곳
+			//통신이나 다른 프로그램과 연결되는 경우에는 특히나 try-catch-finally를 사용
+			//finally는 필수는 아니지만 어떤 경우에도 실행이 되어야 하는 경우에는 사용하여야 함
+			System.out.println("프로그램이 종료되었습니다");
+		}
+		
+		
+		try {
+			findClass();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	//TryCatchFinallyExample의 클래스
+	
+	//발생시키고자하는 예외클래스를 throws를 사용하여 기재한다
+	public static void findClass() throws NullPointerException, ClassNotFoundException {
+		String className = null;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("찾고자 하는 클래스 이름을 적어주세요");
+		className = sc.nextLine();
+		
+		if(className ==null || className.equals("")) {
+			throw new NullPointerException();
+		}
+		
+		Class clazz = Class.forName(className);
+		
+	}
+}
+
